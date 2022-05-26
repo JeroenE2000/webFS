58a3c6160894a3f0a94b125cb3ded066a3f279a4
JeroenE2000 <jmh.elst@student.avans.nl>
2022-05-26 13:11:14 +0200

    styling new website

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+           <div class="content-container text-center">
+                <br>
+                <h3>
+                    Al jaren is De Gouden Draak een begrip als het gaat om de beste afhaalgerechten in 's-Hertogenbosch.<br>
+                    Graag trakteren we u op authentieke gerechten uit de Cantonese keuken.
+                </h3>
+                <br>
+                <h2><u>Speciale Studentenaanbieding</u></h2>
+                <h1>Chinese Rijsttafel (2 personen)</h1>
+                <h3>Maak een keuze uit 3 van onderstaande keuzegerechten:</h3>
+                <br>
+                <br>
+                <div class="row gx-5">
+                    <div class="col text-end">
+                        <h3>Koe Loe Yuk</h3>
+                        <h3>Foe Yong Hai</h3>
+                        <h3>Tjap Tjoy</h3>
+                    </div>
+                    <div class="col text-start">
+                        <h3>Garnalen met Gebakken Knoflook</h3>
+                        <h3>Babi Pangang</h3>
+                        <h3>Kipfilet in Zwarte Bonen saus</h3>
+                    </div>
+                </div>
+                <br>
+                <h1>Met witte rijst. (Nasi of bami voor meerprijs mogelijk.)</h1>
+                <h1>Prijs: €21,00</h1>
+            </div>
+        </div>
+</div>
+@endsection
